852f87778a8453cdc480e85d1401c4a0c0cca245
caroline-ctrl <duon.caroline@gmail.com>
2020-03-25 17:13:20 +0100

    3eme middleware qui affiche dans la console et en html

--- app.js
+++ app.js
@@ -1,16 +1,40 @@
 // ce dont j'ai besoin pour travailler sur cette page
 const express = require('express');
+const bodyParse = require('body-parser');
 
 
 const app = express();
 
+app.use(bodyParse.urlencoded({extended: true}));
+
+// affiche le middleware dans la console et renvoie sur la route /users
 app.get('/', (req, res, next) => {
     console.log('message dans la console');
+    res.redirect('/users');
     next();
 });
 
+
+// affiche le middleware en html (titre et formulaire qui envoie l'action sur la route /create-user)
 app.get('/users', (req, res, next) => {
-    res.send('<form action="/users" method="POST"><input type="texte" name="user"><button type="submit">envoyer</button>');
+    res.send(`
+    <h3>Rentrez votre nom</h3>
+    <form action="/create-user" method="POST"><input type="texte" name="name">
+    <button type="submit">envoyer</button>`
+    );
+    next();
 });
+
+
+// traite le contenu de l'input grace au module body-parser 
+// affiche le middleware dans la console et en html
+app.post('/create-user', (req, res, next) => {
+    console.log(req.body.name);
+    res.send(`
+    <h3>Votre nom est : </h3>
+    <h1>${req.body.name}</h1>`
+    );
+});
+
 
 app.listen(3000);
